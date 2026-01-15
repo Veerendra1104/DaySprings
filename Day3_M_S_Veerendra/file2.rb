254dc202aea21f87802ff14aa7aa7d250bf3d658
M S Veerendra <154041942+Veerendra1104@users.noreply.github.com>
2026-01-15 16:49:15 +0000

    The simple task on data types .

---
+++ Day3_M_S_Veerendra/file2.rb
@@ -0,0 +1,31 @@
+name = "Raju"
+age = 28
+mob = 9876543210
+is_male = true
+height = 6.8
+is_handicap = false
+data = nil
+
+puts name.class
+puts age.class
+puts mob.class
+puts is_male.class
+puts height.class
+puts is_handicap.class
+puts data.class
+
+class Greet
+  puts "Hello From DaySprings ."  
+end
+
+puts Greet.new.class
+
+# String
+# Integer
+# Integer
+# TrueClass
+# Float
+# FalseClass
+# NilClass
+# Hello From DaySprings .
+# Greet
