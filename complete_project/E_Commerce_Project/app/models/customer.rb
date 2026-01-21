388669697ce4b607ee1cd0aa8ec26302a2a7d0f0
M S Veerendra <msveerendraveerendra9954@gmail.com>
2026-01-21 15:27:26 +0530

    Assingment to define a method under the customer and product model .

--- complete_project/E_Commerce_Project/app/models/customer.rb
+++ complete_project/E_Commerce_Project/app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+  def check_email
+    # check email is present or not .
+    email.nil? "Email is not present" : "Email is present ."
+  end
+
+  def name_check
+    # check name has only alphabetic values .
+    name.nil? "name is not present" : "name is present ."
+  end
+
 end
--- complete_project/E_Commerce_Project/app/models/product.rb
+++ complete_project/E_Commerce_Project/app/models/product.rb
@@ -8,4 +8,40 @@ class Product < ApplicationRecord
     # @product = Product.all.limit(10)
     # @stock = Product.all.limit(10).pluck(:stock)
     # @prices = Product.all.limit(10).pluck(:price)
+
+    def  check_availabelity
+        # Check for stock value more than 0 .
+        stock > 10 ? true : false
+    end  
+   
+    def apply_discount(price)
+        # use case statements and calculate discount .
+        @discount = 0
+            case  price
+                when 100..200
+                discount = 10
+                when  201..300
+                discount = 20 
+                when 301..400
+                discount = 30
+                when 401..500
+                discount = 40
+                else
+                discount = 5
+            end
+        
+    end
+
+    def total_amount 
+        # call the  check_availabelity (if true) and then apply_discount use any math(round) method use it  calculate the total .
+        @discount = 0 
+        if check_availabelity? == true
+            @discount = apply_discount(price)
+            return @discount
+        else
+            return false
+
+    end
+
+
 end
